2756fd73c8523c9897aac67612eb942425459a07
kowaitech <kowsalyaaitech@gmail.com>
2026-01-17 12:11:01 +0530

    changes to be

--- frontend/screens/Videoback.js
+++ frontend/screens/Videoback.js
@@ -1,34 +1,3 @@
-// import { View, StyleSheet } from "react-native";
-// import { Video } from "expo-av";
-
-// export default function VideoScreen({children}) {
-//   return (
-//     <View style={styles.container}>
-//       <Video
-//         source={require("../assets/videos/bgvideo.mp4")}
-//         style={styles.video}
-//         resizeMode="contain"
-//         isMuted  
-//         isLooping
-//         shouldPlay
-//       />
-//       {children}
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: "center",
-//   },
-//   video: {
-//     width: "100%",
-//     height: "100%",
-//   },
-// });
-
-
 import { View, StyleSheet } from "react-native";
 import { Video } from "expo-av";
 
